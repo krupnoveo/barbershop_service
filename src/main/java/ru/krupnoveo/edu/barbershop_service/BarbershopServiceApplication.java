41948a9e7e98b2db6501f238949fb3959eddbccf
evgenij <chucknorristhegreat@yandex.ru>
2024-11-28 15:53:57 +0300

    initial commit

---
+++ src/main/java/ru/krupnoveo/edu/barbershop_service/BarbershopServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.krupnoveo.edu.barbershop_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarbershopServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/krupnoveo/edu/barbershop_service/BarbershopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.krupnoveo.edu.barbershop_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
